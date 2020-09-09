06cf1146bb9c7aeeced4cb0d8b234fca2fff7c9d
Marc Lacoursiere <marc@roosoft.com>
2020-09-09 10:03:01 -0400

    added xpriv and xpub

--- main.go
+++ main.go
@@ -8,20 +8,25 @@ import (
 	seed "git.roosoft.com/bitcoin/hd-wallets/1-seed"
 	masterkey "git.roosoft.com/bitcoin/hd-wallets/2-masterPrivateKey"
 	childkeys "git.roosoft.com/bitcoin/hd-wallets/3-childKeys"
+	xpub "git.roosoft.com/bitcoin/hd-wallets/4-xpub"
 )
 
-func main() {
+func doSeed() {
 	fmt.Printf("\n\n1. Seed\n")
 	fmt.Printf("-------\n")
 	fmt.Printf("%-9s %s\n", "mnemonic", seed.GetMnemonic())
 	fmt.Printf("%-9s %x\n", "seed", seed.GetSeed())
+}
 
+func doMasterKey() {
 	fmt.Printf("\n\n2. Master key\n")
 	fmt.Printf("-------------\n")
 	fmt.Printf("%-12s %x\n", "private key", masterkey.GetPrivateKey())
 	fmt.Printf("%-12s %x\n", "chain code", masterkey.GetChainCode())
 	fmt.Printf("%-12s %x\n", "public key", masterkey.GetPublicKey())
+}
 
+func doChildKeys() {
 	fmt.Printf("\n\n3. Child keys\n")
 	fmt.Printf("-------------\n\n")
 	fmt.Printf("---BIP 44 ---\n")
@@ -39,4 +44,22 @@ func main() {
 		path, address, segwit32, segwitNested, _ := childkeys.GetChild(lib.PurposeBIP84, lib.CoinTypeBTC, 0, 0, i)
 		fmt.Printf("%-16s %-34s %-34s %-43s\n", path, address, segwitNested, segwit32)
 	}
+}
+
+func doXpub() {
+	fmt.Printf("\n\n4. xpub\n")
+	fmt.Printf("-------\n\n")
+
+	xpriv := xpub.GetXpriv(lib.PurposeBIP84, lib.CoinTypeBTC, 0, 0, 0)
+	xpub := xpub.GetXpub(lib.PurposeBIP84, lib.CoinTypeBTC, 0, 0, 0)
+
+	fmt.Printf("%-6s %s\n", "xpriv", xpriv)
+	fmt.Printf("%-6s %s\n", "xpub", xpub)
+}
+
+func main() {
+	doSeed()
+	doMasterKey()
+	doChildKeys()
+	doXpub()
 }
